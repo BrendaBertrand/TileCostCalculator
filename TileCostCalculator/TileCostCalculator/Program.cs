cdd8e412e5d6de2a72c44dbcda9f561cd0b89e41
Brenda Bertrand <brenda.bertrand.b@gmail.com>
2024-04-03 21:24:00 +0200

    Basic version of rectangular room calculator

--- TileCostCalculator/TileCostCalculator/Program.cs
+++ TileCostCalculator/TileCostCalculator/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello and Welcome to our Tile Cost Calculator");
+        Console.WriteLine("Hello and Welcome to our Tile Cost Calculator\n");
+        
+        Console.Write("What is the length of the room in meters ? ");
+        double length = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("What is the width of the room in meters ? ");
+        double width = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("What is the cost of 1 square meter of flooring ? ");
+        double unitCost = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine($"\nThe total cost of the flooring is {length*width*unitCost}€");
+        
     }
 }
--- TileCostCalculator/TileCostCalculator/obj/Debug/net8.0/TileCostCalculator.AssemblyInfo.cs
+++ TileCostCalculator/TileCostCalculator/obj/Debug/net8.0/TileCostCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TileCostCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb3076957e3f5a033d2a9fc2ee053bb31712f7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16bc7a365ec3b21913c7cf398f740823fa1f0234")]
 [assembly: System.Reflection.AssemblyProductAttribute("TileCostCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TileCostCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
