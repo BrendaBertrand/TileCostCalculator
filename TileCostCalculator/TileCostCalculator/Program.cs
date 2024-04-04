2e02d1f7c69721eb8fdda55a614e2514357c459c
Brenda Bertrand <brenda.bertrand.b@gmail.com>
2024-04-04 23:17:40 +0200

    Refactoring of the code based on feedback (use of TryParse, correct location of variables' declaration)

--- TileCostCalculator/TileCostCalculator/Program.cs
+++ TileCostCalculator/TileCostCalculator/Program.cs
@@ -2,93 +2,79 @@
 
 class Program
 {
-    static double getPositiveDouble(string question)
+    const int HOURLY_RATE = 86;
+    const int HOURLY_AREA = 2;
+    const int NUMBER_OF_DECIMALS = 2;
+    static readonly string[] SHAPE_LIST = ["Rectangular", "Round" ];
+
+    static double GetPositiveDouble(string question)
     {
-        bool inputTypeisCorrect = false;
         double value = 0;
         Console.Write(question);
-        do
+        while (!Double.TryParse(Console.ReadLine(), out value) || value <= 0)
         {
-            try
-            {
-                value = Convert.ToDouble(Console.ReadLine());
-                if (value <= 0)
-                {
-                    Console.WriteLine("Please enter a positive number");
-                }
-                else
-                {
-                    inputTypeisCorrect = true;
-                }
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine("Please enter a number");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Something went wrong : {e.Message}");
-                Console.WriteLine("Please enter your number again");
-            }
-        } while (!inputTypeisCorrect);
+            Console.WriteLine("Please enter a positive number");
+        }
 
         return value;
     }
 
     static void Main(string[] args)
     {
-        double[] dimensions = new double[2];
-        double flooringUnitCost = 0;
-        string shape = "";
-        double area = 0.0;
-        const int hourlyRate = 86;
-        const int hourlyArea = 2;
-        double workedHours = 0.0;
-        double laborCost = 0;
-        double flooringCost = 0;
-
+       
+        Dictionary<string, string> shapes = new Dictionary<string, string>()
+        {
+            { SHAPE_LIST[0], "1" },
+            { SHAPE_LIST[1], "2" }
+        };
 
         Console.WriteLine("Hello and Welcome to our Tile Cost Calculator\n");
 
-        flooringUnitCost = getPositiveDouble("What is the cost of 1 square meter of flooring ? ");
+        double flooringUnitCost = 0;
+        flooringUnitCost = GetPositiveDouble("What is the cost of 1 square meter of flooring ? ");
 
 
-        Console.WriteLine("What is the shape of you room ? \n1 - Rectangular\n2 - Round");
-
+        string selectedShape = "";
         do
         {
-            shape = Console.ReadLine();
-            if (shape != "1" && shape != "2")
+            Console.WriteLine($"\nWhat is the shape of you room ?");
+            foreach (var shape in shapes)
             {
-                Console.WriteLine("Please enter 1 or 2");
-                shape = "";
+                Console.WriteLine($"Enter {shape.Value} for {shape.Key}");
             }
-        } while (shape != "1" && shape != "2");
 
+            selectedShape = Console.ReadLine();
+        } while (!shapes.ContainsValue(selectedShape));
 
-        if (shape == "1")
+        double[] dimensions = new double[2];
+        double area = 0.0;
+
+        if (selectedShape == shapes[SHAPE_LIST[0]])
         {
-            dimensions[0] = getPositiveDouble("What is the length of the room in meters ? ");
-            dimensions[1] = getPositiveDouble("What is the width of the room in meters ? ");
+            dimensions[0] = GetPositiveDouble("\nWhat is the length of the room in meters ? ");
+            dimensions[1] = GetPositiveDouble("\nWhat is the width of the room in meters ? ");
 
             area = dimensions[0] * dimensions[1];
         }
 
-        if (shape == "2")
+        if (selectedShape == SHAPE_LIST[1])
         {
-            dimensions[0] = getPositiveDouble("What is the radius of the room in meters ? ");
+            dimensions[0] = GetPositiveDouble("\nWhat is the radius of the room in meters ? ");
 
             area = dimensions[0] * dimensions[0] * Math.PI;
         }
 
-        workedHours = Math.Ceiling(area / hourlyArea);
+        double workedHours = 0.0;
+        double flooringCost = 0;
+        double laborCost = 0;
+        workedHours = Math.Ceiling(area / HOURLY_AREA);
 
-        laborCost = Math.Round(workedHours * hourlyRate, 2);
-        flooringCost = Math.Round(area * flooringUnitCost, 2);
+        laborCost = Math.Round(workedHours * HOURLY_RATE, NUMBER_OF_DECIMALS);
+        flooringCost = Math.Round(area * flooringUnitCost, NUMBER_OF_DECIMALS);
 
 
         Console.WriteLine($"\nThe cost of the flooring is {flooringCost}€");
-        Console.WriteLine($"\nThe cost of the workmanship is {laborCost}€");
+        Console.WriteLine($"The cost of the workmanship is {laborCost}€");
         Console.WriteLine($"\nThe total cost is {laborCost + flooringCost}€");
     }
 }
